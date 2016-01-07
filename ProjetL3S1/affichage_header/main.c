06fe3139ce6a664c5544ddf990783dd25f613efa
alex.daubois <alex.daubois@gmail.com>
2016-01-07 14:03:05 +0100

    Résolution de #2

--- ProjetL3S1/affichage_header/main.c
+++ ProjetL3S1/affichage_header/main.c
@@ -1,16 +1,7 @@
 #include "compute_args.h"
 
 int main(int argc, char **argv) {
-    int return_value = EXIT_SUCCESS;
-
-    if (argc < 2) {
-        print_help();
-        return ERROR_MISSING_ARG;
-    }
-
-    if (argc >= 3) {
-        return_value = compute_multiple_args(argc, argv);
-    }
+    int return_value = compute_multiple_args(argc, argv);
 
     return return_value;
 }
