87d8424540d80d0556bcdaed13c1eed978b21cf6
BuildTools <unconfigured@null.spigotmc.org>
2016-01-08 11:19:57 +0100

    traduction en anglais des affichages printelf

--- ProjetL3S1/affichage/printelf.h
+++ ProjetL3S1/affichage/printelf.h
@@ -2,16 +2,23 @@
 #define PRINTELF_H
 
 #include "../commun/readelf.h"
+/*
 
+	Print the informations about the format of the file ( Magic number, Endian ...)
+
+*/
 int print_elf_header(Elf32_Ehdr elf);
+
 void print_elf_symbol_table(Elf32_Sym *symbols, uint16_t symbols_count);
 
 void print_elf_section_header(Elf32_Ehdr header, Elf32_Shdr * table_entetes_section, char *c);
 
 void print_elf_rel_tab(TableRel * tab, Elf32_Shdr * table_entetes_section, char *secname);
+
 void print_elf_rela_tab(TableRela * tab);
 
 void print_elf_section_header(Elf32_Ehdr header, Elf32_Shdr * table_entetes_section, char *secname);
+
 void print_elf_section_content(uint8_t** secContent, int number, Elf32_Shdr *section_headers, char *secname, Elf32_Ehdr elf_header);
 
 #define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
--- ProjetL3S1/affichage_header/main.c
+++
@@ -1,7 +0,0 @@
-#include "compute_args.h"
-
-int main(int argc, char **argv) {
-    int return_value = compute_multiple_args(argc, argv);
-
-    return return_value;
-}
