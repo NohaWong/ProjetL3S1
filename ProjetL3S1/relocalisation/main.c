5efb927496819631ab9dc9304f63b6f0510121ce
Sylvain Marion <s-marion@hotmail.fr>
2016-01-12 10:20:21 +0100

    Nettoyage du main et merge.

--- ProjetL3S1/relocalisation/main.c
+++ ProjetL3S1/relocalisation/main.c
@@ -4,8 +4,6 @@
 #include "write_file.h"
 
 
-
-
 // APPEL : relocalisation <nom_fichier> <nom_fichier_cible> <nom_section adresse_relocalisee> [nom_section_x adresse_relocalisee_x]
 
 int main(int argc, char **argv) {
@@ -36,91 +34,31 @@ int main(int argc, char **argv) {
        return EXIT_FAILURE;
     }
 
-    Elf32_Ehdr header;
-
+    Elf32_Ehdr header, new_header;
     Elf32_Shdr *section_header_table = NULL;
-
-    Elf32_Ehdr* new_header= malloc(sizeof(Elf32_Ehdr));
-
-
-    char *table_nom_sections = NULL;
+    char *section_header_name = NULL;
     uint16_t symbols_count = 0;
     uint8_t **section_content;
-    Elf32_Sym *new_symb_table;
 
     // load everything before printing
     read_elf_header(file_source, &header);
 
-    memcpy(new_header, &header, sizeof(Elf32_Ehdr));
-    write_file_header(file_target,new_header);
+ //   memcpy(new_header, &header, sizeof(Elf32_Ehdr));
+//    write_file_header(file_target, &new_header);
 
 
-    // check if magic numbers are correct or not
-    if (    header.e_ident[EI_MAG0] != ELFMAG0
-        ||  header.e_ident[EI_MAG1] != ELFMAG1
-        ||  header.e_ident[EI_MAG2] != ELFMAG2
-        ||  header.e_ident[EI_MAG3] != ELFMAG3) {
-            printf("Erreur : Les nombres magiques ne sont pas corrects.\n");
-            return ERROR_MAGIC_NUMBERS;
-    }
-
-
-
-    section_header_table = read_elf_section_header(file_source, &header, &table_nom_sections);
+    section_header_table = read_elf_section_header(file_source, &header, &section_header_name);
     Elf32_Sym *symbols = read_symbol_table(file_source, section_header_table,/* header.e_shnum,*/ &symbols_count);
     section_content = read_section_content(file_source, section_header_table, &header);
     Ensemble_table_rel table_rel= read_rel_table(file_source, section_header_table, header.e_shnum);
 
-    printf("#      Nom         Valeur      Type      Portée    Indice de section\n");
-        printf("--------------------------------------------------------------------\n");
-        for (i = 0; i < symbols_count; ++i) {
-            switch (ELF32_ST_TYPE(symbols[i].st_info)) {
-                case STT_NOTYPE:
-                default:
-                    strcpy(type, "NOTYPE");
-                    break;
-                case STT_OBJECT:
-                    strcpy(type, "OBJECT");
-                    break;
-                case STT_FUNC:
-                    strcpy(type, "FUNC");
-                    break;
-                case STT_SECTION:
-                    strcpy(type, "SECTION");
-                    break;
-                case STT_FILE:
-                    strcpy(type, "FILE");
-                    break;
-            }
 
-            //bind
-            switch(ELF32_ST_BIND(symbols[i].st_info)) {
-                case STB_LOCAL:
-                    strcpy(info, "LOCAL");
-                    break;
-                case STB_GLOBAL:
-                    strcpy(info, "GLOBAL");
-                    break;
-                case STB_WEAK:
-                    strcpy(info, "WEAK");
-                    break;
-                case STB_NUM:
-                    strcpy(info, "NUM");
-                    break;
-                default:
-                    strcpy(info, "OTHER");
-                    break;
-            }
-
-            printf("%-7d%#-12x%#-12x%-10s%-10s%-2d", i, symbols[i].st_name, symbols[i].st_value, type, info, symbols[i].st_shndx);
-            printf("\n");
-        }
-        printf("\n");
-
-
-
+    // get args
+    if (!(argc%2)) {
+        printf("Mauvais nombre d'arguments.");
+        return 0;
+    }
     uint32_t rel_count = (argc-2)/2;
-
 
     rel_info* table_rel_info;
     table_rel_info = malloc(rel_count * sizeof(rel_info));
@@ -131,13 +69,65 @@ int main(int argc, char **argv) {
         table_rel_info[i].section_new_addr = strtol(argv[((2*i)+1)+3], NULL, 16);
     }
 
-    new_section_header(table_entetes_section, table_nom_sections, table_rel_info, rel_count, header);
 
-    Elf32_Shdr * new_sections_header = new_section_header(section_header_table, table_nom_sections, table_rel_info, nb_relocalisation, header,new_header);
+    // create new structs
+    new_header = header;
+    Elf32_Shdr *new_sections_header = new_section_header(section_header_table, section_header_name, table_rel_info, rel_count, header, &new_header);
+    uint8_t **new_section = new_section_content (table_rel, section_header_name, section_content, table_rel_info, section_header_table, header, rel_count, symbols);
+    Elf32_Sym *new_symb_table= new_symbol_table(symbols, table_rel_info, symbols_count, rel_count, section_header_table, section_header_name);
+
+
+
+    // Affichages tests
+    printf("#      Nom         Valeur      Type      Portée    Indice de section\n");
+    printf("--------------------------------------------------------------------\n");
+    for (i = 0; i < symbols_count; ++i) {
+        switch (ELF32_ST_TYPE(symbols[i].st_info)) {
+            case STT_NOTYPE:
+            default:
+                strcpy(type, "NOTYPE");
+                break;
+            case STT_OBJECT:
+                strcpy(type, "OBJECT");
+                break;
+            case STT_FUNC:
+                strcpy(type, "FUNC");
+                break;
+            case STT_SECTION:
+                strcpy(type, "SECTION");
+                break;
+            case STT_FILE:
+                strcpy(type, "FILE");
+                break;
+        }
+
+        //bind
+        switch(ELF32_ST_BIND(symbols[i].st_info)) {
+            case STB_LOCAL:
+                strcpy(info, "LOCAL");
+                break;
+            case STB_GLOBAL:
+                strcpy(info, "GLOBAL");
+                break;
+            case STB_WEAK:
+                strcpy(info, "WEAK");
+                break;
+            case STB_NUM:
+                strcpy(info, "NUM");
+                break;
+            default:
+                strcpy(info, "OTHER");
+                break;
+        }
+
+        printf("%-7d%#-12x%#-12x%-10s%-10s%-2d", i, symbols[i].st_name, symbols[i].st_value, type, info, symbols[i].st_shndx);
+        printf("\n");
+    }
+    printf("\n");
 
     for (i=0; i < header.e_shnum; i++) {
         printf("%-6d%-20s%#-12x%#-8x%#-8x(+ %#-8x) %#-8x%#-8x%#-11x%#-8x", i,
-                           &(table_nom_sections[section_header_table[i].sh_name]),
+                           &(section_header_name[section_header_table[i].sh_name]),
                            section_header_table[i].sh_type,
                            section_header_table[i].sh_flags,
                            section_header_table[i].sh_addr,
@@ -150,9 +140,6 @@ int main(int argc, char **argv) {
 
         printf("\n");
     }
-    new_section_content (table_rel,table_nom_sections,section_content,table_rel_info, section_header_table, &header,nb_relocalisation,symbols);
-    new_symb_table= new_symbol_table(symbols, table_rel_info, symbols_count, nb_relocalisation, section_header_table, table_nom_sections);
-
 
     printf("#      Nom         Valeur      Type      Portée    Indice de section\n");
         printf("--------------------------------------------------------------------\n");
@@ -200,12 +187,9 @@ int main(int argc, char **argv) {
         }
         printf("\n");
 
-    free(new_symb_table);
-
-
-    for (i=0; i < header.e_shnum; i++) {
+    for (i=0; i < new_header.e_shnum; i++) {
         printf("%-6d%-20s%#-12x%#-8x%#-8x(+ %#-8x) %#-8x%#-8x%#-11x%#-8x", i,
-                           &(table_nom_sections[new_sections_header[i].sh_name]),
+                           &(section_header_name[new_sections_header[i].sh_name]),
                            new_sections_header[i].sh_type,
                            new_sections_header[i].sh_flags,
                            new_sections_header[i].sh_addr,
@@ -219,16 +203,11 @@ int main(int argc, char **argv) {
         printf("\n");
     }
 
-    uint8_t **new_section = new_section_content (table_rel, table_nom_sections, section_content, table_rel_info, table_entetes_section, header, rel_count, symbols);
-
-    //write_file_header(file_target,new_header);
-
-    new_section_content (table_rel,table_nom_sections,section_content,table_rel_info, section_header_table, &header,nb_relocalisation,symbols);
-
-
     free(symbols);
+    free(new_symb_table);
     free(section_header_table);
-    free(table_nom_sections);
+    free(new_sections_header);
+    free(section_header_name);
 
     for (i = 0; i < header.e_shnum; ++i) {
         free(new_section[i]);
@@ -237,15 +216,10 @@ int main(int argc, char **argv) {
 
 
     fclose(file_source);
-    fclose(file_target);//    free(table_rela);
+    fclose(file_target);
 
-    free(new_sections_header);
-    free(new_header);
-
-//    free(table_rela);
-//    free(table_rel);
-//    free(table_rel_info);
-//    free(section_content);
+    free(table_rel_info);
+    free(section_content);
 
     return 0;
 }
