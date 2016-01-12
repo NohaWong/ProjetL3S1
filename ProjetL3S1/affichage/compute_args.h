dd9594ba91e6e9aa92aa102e0e1dce42e0f721aa
alex.daubois <alex.daubois@gmail.com>
2016-01-12 13:26:33 +0100

    Séparation du programme, entre le main et compute_args

--- ProjetL3S1/affichage/compute_args.h
+++ ProjetL3S1/affichage/compute_args.h
@@ -4,6 +4,23 @@
 #include "../commun/readelf.h"
 #include "printelf.h"
 
+
+/**
+ * @struct main_flags
+ *
+ * Holds all flags given to main
+ */
+typedef struct {
+    int     hflag; // header
+    int     sflag; // symbols
+    int     Sflag; // sections
+    int     xflag; // hex-dump
+    char*   xflag_arg; // hex-dump arg
+    int     rflag; // relocs
+    int     is_xflagarg_int;
+} main_flags;
+
+
 /**
  * This function retrieve all arguments given in command line by the @a main
  * function.
@@ -13,7 +30,7 @@
  * @return Return a code defined is the error enumeration in @a printfelf.h
  *
  */
-int compute_multiple_args (int argc, char **argv);
+int compute_multiple_args (int argc, char **argv, main_flags *flags, int *argv_idx_filename);
 
 /**
  * @todo
